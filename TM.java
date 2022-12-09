2aec4740d0407af66fd6ceb80f1870fb9657ab22
tommytro <85188391+tommytro@users.noreply.github.com>
2022-12-09 10:15:00 -0700

    changed some things

--- TM.java
+++ TM.java
@@ -1,3 +1,4 @@
+
 import java.util.LinkedHashSet;
 import java.util.Set;
 /**
--- TMSimulator.java
+++ TMSimulator.java
@@ -1,3 +1,5 @@
+
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashSet;
@@ -12,35 +14,66 @@ public class TMSimulator {
 		if(file.exists()){
 			//create a DFA instance -- you need to write DFA.java that
 			//implements DFAInterface.java
-			TM tm = new TM();
+			// TM tm = new TM();
+            int numStates = 0;
+            int symbols = 0;
+            int acceptStates = 0;
+            String string = "";
+            String currLine = "";
+            String lastLine = "";
+            int counter = 0;
+
 			Scanner scan = new Scanner(file);
-			//the first line is the set of final states
-			//get the string of the final states and split it on a space
-			String finalStates = scan.nextLine().trim();
-			//using tokenizer to split the string
-			StringTokenizer tk = new StringTokenizer(finalStates, " ");
-			while(tk.hasMoreTokens()){
-				tm.addFinalState(tk.nextToken());
-			}
+
+            while((currLine = scan.nextLine().trim()) != null){
+                if(counter == 0){
+                    //the first line is the number of states in the TM
+                    numStates = Integer.parseInt(currLine);
+                }else if(counter == 1){
+                    //the second line is the number of symbols in the language
+                    symbols = Integer.parseInt(currLine);
+                }else if(counter > 1){
+                    //Add
+
+
+                }
+                System.out.println(currLine);
+                lastLine = currLine;
+                counter++;
+            }
+            string = lastLine;
+
+            
+            // //the second line is the number of symbols in the language
+            // symbols = Integer.parseInt(scan.nextLine().trim());
+
+            // String startStateName = scan.nextLine().trim();
+            // tm.addStartState(startStateName);
+
+
+
+			// //using tokenizer to split the string
+			// StringTokenizer tk = new StringTokenizer(finalStates, " ");
+			// while(tk.hasMoreTokens()){
+			// 	tm.addFinalState(tk.nextToken());
+			// }
 			
-			//the second line is the start state
-			String startStateName = scan.nextLine().trim();
-			tm.addStartState(startStateName);
+			
 
-			//get the string of other states and split in on space too
-			String otherStates = scan.nextLine().trim();
-			tk = new StringTokenizer(otherStates, " ");
-			while(tk.hasMoreTokens()){
-				tm.addState(tk.nextToken());
-			}
+			// //get the string of other states and split in on space too
+			// String otherStates = scan.nextLine().trim();
+			// tk = new StringTokenizer(otherStates, " ");
+			// while(tk.hasMoreTokens()){
+			// 	tm.addState(tk.nextToken());
+			// }
 
-			//read in the transactions
-			String trans = scan.nextLine();
-			tk = new StringTokenizer(trans, " ");
-			while(tk.hasMoreTokens()){
-				char[] tran = tk.nextToken().toCharArray();
-				tm.addTransition(String.valueOf(tran[0]), tran[1], String.valueOf(tran[2]));
-			}
+			// //read in the transactions
+			// String trans = scan.nextLine();
+			// tk = new StringTokenizer(trans, " ");
+			// while(tk.hasMoreTokens()){
+			// 	char[] tran = tk.nextToken().toCharArray();
+			// 	tm.addTransition(String.valueOf(tran[0]), tran[1], String.valueOf(tran[2]));
+			// }
 			scan.close();
 		} else {
 			System.err.println(file + " does not exists - please check the file path");
